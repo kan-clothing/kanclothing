973ed756ee82a94769682739128525e07a66b8e8
kan-clothing <132319193+kan-clothing@users.noreply.github.com>
2023-05-02 21:28:02 +0800

    Add files via upload
    testing php

---
+++ testlang.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+    </head>
+  <body>
+
+    <?php
+       $age = array("Peter"=>"35","Ben"=>"37","Joe"=>"43");
+       krsort($age); 
+
+       foreach($age as $x=> $x_value){
+        echo "Key=" . $x . ", Value=" .$x_value;
+        echo "<br>";
+       }
+     ?>
+     
+</body>
+
+</html>
