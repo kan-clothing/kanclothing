dbbb0248178fcc6c284f2e283c5e4713c7c60e28
kan-clothing <132319193+kan-clothing@users.noreply.github.com>
2023-05-23 16:52:01 +0800

    smtp implementation

---
+++ js/livesearch.php
@@ -0,0 +1,30 @@
+<?php
+$xmlDoc = new DOMDocument();
+$xmlDoc->load("links.xml");
+$x = $xmlDoc->getElementsByTagName('link');
+$q = $_GET["q"];
+
+if (strlen($q) > 0) {
+    $hint = "";
+  
+    for ($i = 0; $i < $x->length; $i++) {
+        $y = $x->item($i)->getElementsByTagName('title');
+        $z = $x->item($i)->getElementsByTagName('url');
+    
+        if ($y->item(0) != null && $y->item(0)->nodeType == 1) {
+            $title = $y->item(0)->nodeValue;
+      
+            if (stripos($title, $q) !== false) {
+                $hint .= "<a href='" .
+                    $z->item(0)->nodeValue .
+                    "' target='_blank' style='padding-right: 20px; width: 100%; font-size: 15px; color: #666666; padding-left: 20px; border: 1px solid #e5e5e5; height: 42px;'>" .
+                    $title .
+                    "</a><br />";
+            }
+        }
+    }
+}
+
+$response = ($hint != "") ? $hint : "<div style='padding: 20px; font-size: 15px; color: #666666;'>No suggestions</div>";
+echo $response;
+?>
--- js/main.js
+++ js/main.js
@@ -242,6 +242,23 @@
         }
         
 
+        function showResult(str) {
+            if (str.length == 0) {
+                document.getElementById("livesearch2").innerHTML = "";
+                document.getElementById("livesearch2").style.border = "0px";
+                return;
+            }
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.getElementById("livesearch2").innerHTML = this.responseText;
+                    document.getElementById("livesearch2").style.border = "1px solid #A5ACB2";
+                }
+            }
+            xmlhttp.open("GET", "path/to/livesearch.php?q=" + str, true);
+            xmlhttp.send();
+        }
+
 
 
 })(jQuery);
