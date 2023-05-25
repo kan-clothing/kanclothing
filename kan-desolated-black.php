c0e9a370fe5fbdaffa4f8942f41cca560e5e512c
kan-clothing <132319193+kan-clothing@users.noreply.github.com>
2023-05-25 10:57:41 +0800

    Update kan-desolated-black.php

--- kan-desolated-black.php
+++ kan-desolated-black.php
@@ -158,7 +158,7 @@
                                 <i class="fa fa-star-o"></i>
                                 <span> - 0 Reviews</span>
                             </div>
-                            <h3>₱549.00</h3>
+                            <h3>₱410.00</h3>
                             <p>This is a silkscreen-printed T-Shirt with the logo printed on the front, and an unknown girl face on the back. This is made out of 100% cotton, providing comfort and style to your fit.
                             </p>
                             <div class="product__details__option">
