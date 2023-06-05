dd56eb4f44bd4abc0967dcbc27587987a537bcfc
megel <132319193+kan-clothing@users.noreply.github.com>
2023-06-06 01:09:38 +0800

    a

--- js/loggedin.js
+++ js/loggedin.js
@@ -1,30 +1,5 @@
-try {
-  firebase.initializeApp(firebaseConfig);
-} catch (error) {
-  if (error.code === "app/duplicate-app") {
-    window.location.href = 'index.html';
-  } else {
-    console.log('Firebase initialization error:', error);
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    window.location.href = "index.html";
   }
-}
-
-const auth = firebase.auth();
-const database = firebase.database();
-
-function loadLoginScript() {
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "js/login.js", true);
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      eval(xhr.responseText);
-
-      // Check if user is already logged in
-      if (isLoggedIn) {
-        window.location.href = 'index.html';
-      }
-    }
-  };
-  xhr.send();
-}
-
-loadLoginScript();
+});
