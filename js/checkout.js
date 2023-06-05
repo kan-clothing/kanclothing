6d47cd041b043f11462b26fce228b094713fda33
megel <132319193+kan-clothing@users.noreply.github.com>
2023-06-05 19:23:42 +0800

    lipat smtp sa email ni banag

--- js/checkout.js
+++ js/checkout.js
@@ -10,19 +10,21 @@ btn.addEventListener('click', function(e) {
     var postal = document.getElementById('postcode').value;
     var phone = document.getElementById('phone').value;
 	
-	var mail ='SUBJECT: CHECKOUT ' +  '<br/> ORDERS: Wala pa haha (place holder lang e2)' + '</br>NAME: '+ sname + ' '+ fname +'<br/> EMAIL: ' + email +'</br> PHONE: ' + phone +'</br> POSTCODE: ' + postal +'<br/> NOTES: ' + message1;
+	var mail ='SUBJECT: CHECKOUT ' +  '<br/> ORDERS: Wala pa haha (place holder lang e2)' + '</br> NAME: '+ sname + ' '+ fname +'<br/> EMAIL: ' + email +'</br> PHONE: ' + phone +'</br> POSTCODE: ' + postal +'<br/> NOTES: ' + message1;
 	           
 	
 	
     Email.send({
-    Host : "smtp.elasticemail.com",
-    Username : "accscent@outlook.ph",
-    Password : "5AB00F3056B9D60C0369328869A353C6F8A7",
-    To :  "accscent@outlook.ph",
-    From : "gabenone7@gmail.com",
-    Subject : 'CHECKOUT',
-    Body : mail,
-}).then(
-    message => alert(message)
-);	
-	})  
+        Host: "smtp.elasticemail.com",
+        Username: "kanclothingph1@gmail.com",
+        Password: "5A9752781AFC5E4478BF8F10BB7AACF32FDC",
+        To: "kanclothingph1@gmail.com",
+        From: "kanclothingph1@gmail.com",
+        Subject: 'CHECKOUT',
+        Body: mail,
+    }).then(
+        message => alert("Email sent successfully")
+    ).catch(
+        error => alert("Error: " + error)
+    );
+});
--- js/sendmail.js
+++ js/sendmail.js
@@ -1,26 +1,26 @@
 var btn = document.getElementById('btnsend');
 btn.addEventListener('click', function(e) {
-    e.preventDefault()
-		alert("If the next message box says OK, your email has been sent successfully. Make sure you have entered a valid email, we'll reply as soon as possible (our email may be marked as spam, please check spam). Have a nice day!");
-	
-	var name = document.getElementById('name').value;
-	var email = document.getElementById('email').value;
-	var subject = document.getElementById('subject').value;
-	var message1 = document.getElementById('message').value;
-	
-	var mail = 'name: '+name +'<br/> email: ' + email + '<br/> subject: ' +  subject + '<br/> message: ' + message1;
-	           
-	
-	
+    e.preventDefault();
+    alert("If the next message box says OK, your email has been sent successfully. Make sure you have entered a valid email; we'll reply as soon as possible (our email may be marked as spam, please check spam). Have a nice day!");
+
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var subject = document.getElementById('subject').value;
+    var message1 = document.getElementById('message').value;
+
+    var mail = 'name: ' + name + '<br/> email: ' + email + '<br/> subject: ' + subject + '<br/> message: ' + message1;
+
     Email.send({
-    Host : "smtp.elasticemail.com",
-    Username : "accscent@outlook.ph",
-    Password : "5AB00F3056B9D60C0369328869A353C6F8A7",
-    To :  "accscent@outlook.ph",
-    From : "gabenone7@gmail.com",
-    Subject : subject,
-    Body : mail,
-}).then(
-    message => alert(message)
-);	
-	})
+        Host: "smtp.elasticemail.com",
+        Username: "kanclothingph1@gmail.com",
+        Password: "5A9752781AFC5E4478BF8F10BB7AACF32FDC",
+        To: "kanclothingph1@gmail.com",
+        From: "kanclothingph1@gmail.com",
+        Subject: subject,
+        Body: mail,
+    }).then(
+        message => alert("Email sent successfully")
+    ).catch(
+        error => alert("Error: " + error)
+    );
+});
