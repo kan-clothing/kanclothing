fd2214447be5322a932bf455b12edd43308a4d79
megel <132319193+kan-clothing@users.noreply.github.com>
2023-06-08 01:36:44 +0800

    sandamakmak na ewan
    pakamatai na

--- js/checkout.js
+++ js/checkout.js
@@ -1,19 +1,53 @@
 var btn = document.getElementById('checkout');
 btn.addEventListener('click', function(e) {
-    e.preventDefault()
-		alert("If the next message box says OK, your email has been sent successfully. Make sure you have entered a valid email, we'll reply as soon as possible (our email may be marked as spam, please check spam). Have a nice day!");
-	
-	var sname = document.getElementById('fname').value;
+    e.preventDefault();
+
+    // Alert message
+    alert("If the next message box says OK, your email has been sent successfully. Make sure you have entered a valid email, we'll reply as soon as possible (our email may be marked as spam, please check spam). Have a nice day!");
+
+    // Retrieve form input values
+    var sname = document.getElementById('fname').value;
     var fname = document.getElementById('sname').value;
-	var email = document.getElementById('email').value;
-	var message1 = document.getElementById('message').value;
+    var email = document.getElementById('email').value;
+    var message1 = document.getElementById('message').value;
+    var city = document.getElementById('tcity').value;
     var postal = document.getElementById('postcode').value;
     var phone = document.getElementById('phone').value;
-	
-	var mail ='SUBJECT: CHECKOUT ' +  '<br/> ORDERS: Wala pa haha (place holder lang e2)' + '</br> NAME: '+ sname + ' '+ fname +'<br/> EMAIL: ' + email +'</br> PHONE: ' + phone +'</br> POSTCODE: ' + postal +'<br/> NOTES: ' + message1;
-	           
-	
-	
+    var address1 = document.getElementById('address1').value;
+    var address2 = document.getElementById('address2').value;
+
+    // Validate required fields
+    if (sname === '' || fname === '' || email === '' || city === '' || postal === '' || phone === '' || address1 === '') {
+        alert('Please fill in all required fields.');
+        return;
+    }
+
+    // Validate email format
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        alert('Please enter a valid email address.');
+        return;
+    }
+
+    // Validate phone number format
+    var phoneRegex = /^09\d{9}$/;
+    if (!phoneRegex.test(phone)) {
+        alert('Please enter a valid phone number (11 digits starting with 09).');
+        return;
+    }
+
+    // Construct email body
+    var mail = 'SUBJECT: CHECKOUT <br>' +
+               'ORDERS: NOTHING YET <br>' +
+               'ACCUMULATED PRICE: NOTHING YET <br>' +
+               'NAME: ' + sname + ' ' + fname + '<br>' +
+               'EMAIL: ' + email + '<br>' +
+               'PHONE: ' + phone + '<br>' +
+               'ADDRESS: ' + address1 + ' ' + address2 + '<br>' +
+               'POSTCODE: ' + postal + '<br>' +
+               'NOTES: ' + message1;
+
+    // Send email
     Email.send({
         Host: "smtp.elasticemail.com",
         Username: "kanclothingph1@gmail.com",
